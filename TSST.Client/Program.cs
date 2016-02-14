ea592bf69ae46706adb96de98694ae826908ce24
Jakub Bańka <kubabanka@gmail.com>
2016-02-14 23:35:43 +0100

    Added: -Getting token before main form shows

--- TSST.Client/Program.cs
+++ TSST.Client/Program.cs
@@ -11,7 +11,31 @@ namespace TSST.Client
    
     static class Program
     {
-        
+        static private async Task getAccessToken()
+        {
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                {
+                        { "grant_type", "facebook" },
+                        { "accesstoken", Properties.Settings.Default.FacebookToken }
+                };
+                var content = new FormUrlEncodedContent(values);
+                var response = await client.PostAsync(Properties.Settings.Default.ServiceUri+ "/Token", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var postResponse = new JavaScriptSerializer().Deserialize<PostResponse>(responseString);
+                    Properties.Settings.Default.ApiToken = postResponse.access_token;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+
+            }
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -24,7 +48,8 @@ namespace TSST.Client
 
             if (fLogin.ShowDialog() == DialogResult.OK)
             {
-               
+                var getToken =  getAccessToken();
+                getToken.Wait();
                 //   MessageBox.Show(Properties.Settings.Default.FacebookToken);
                 Application.Run(new MainForm());
             }
